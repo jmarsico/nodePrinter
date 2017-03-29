97486646b9a5f7dd8c0333bc3a126a7cc392e965
Jakob Marsico <jmarsico@andrew.cmu.edu>
2017-03-29 10:31:53 -0400

    small change for debuggin

--- printer.js
+++ printer.js
@@ -6,21 +6,22 @@ var cmd=require('node-cmd');
 var inport = 12345;
 console.log ("OSC listener running at http://localhost:" + inport);
 
-var printerName;
+var printerName = 'Canon_MG3600_series_2';
 var printCommand = 'lp -d';
-var fileName = 'test.txt';
+var fileName = 'pages.pdf';
 
 sock = udp.createSocket("udp4", function(msg, rinfo) {
   var error, error1;
   try {
   	var message = osc.fromBuffer(msg);
+    console.log('message received: ' + message.address + ' ' + message.args[0].value);
   	if(message.address == '/printer'){
   		printerName = message.args[0].value;
   		printCommand = 'lp -d ' + printerName + ' ' + fileName;
-  		console.log(printCommand);
+  		console.log('COMMAND: ' + ' ' +printCommand);
 
   		cmd.run(printCommand);
-  		return console.log(message);
+  		// return console.log(message);
   	}
 
   	
